3399e67778efb6559883a2e42ccdfa80d28b53c4
Alex Ermolin <alex@ermol.in>
2025-03-31 02:16:48 +0300

    master: base skeleton

---
+++ bin/supervisord.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env zsh
+
+builtin cd "$(dirname "$0")/../"
+
+if [ "$?" -eq 0 ]; then
+
+    # check if supervisord is already running
+
+    if [ -f "run/supervisord.pid" ]; then
+        pid="$(pgrep -F "run/supervisord.pid")"
+        if [ -n "$pid" ] && [ "$pid" -gt 0 ] && [ -S "run/supervisord.sock" ]; then
+            echo " ++ warn ($0): supervisord($pid) is already running (from $PWD/run/supervisord.pid)" >&2
+            return 0
+        fi
+    fi
+
+    # evaluate local .env when exists before supervisord
+
+    if [ -x $commands[direnv] ]; then
+        eval "$(direnv export zsh)"
+    fi
+
+    # start supervisord and check if it is started successfully
+    uvx --from supervisor supervisord --silent --configuration $PWD/supervisord.conf && sleep 0.5
+
+    if [ "$?" -eq 0 ] && [ -f "run/supervisord.pid" ]; then
+        pid="$(pgrep -F "run/supervisord.pid")"
+        if [ -n "$pid" ] && [ "$pid" -gt 0 ] && [ -S "run/supervisord.sock" ]; then
+            echo " -- info ($0): supervisord($pid) started (from $PWD/run/supervisord.pid)" >&2
+            return 0
+        fi
+    fi
+
+    echo " == fail ($0): supervisord start failed" >&2
+    return 1
+fi
