24bab7eb4a8754762e317a05ab468f2260c07791
Alex Kalaverin <alex@kalaver.in>
2025-07-03 21:47:30 +0000

    master: (supervisor) update runner

--- bin/supervisord.sh
+++ bin/supervisord.sh
@@ -6,22 +6,36 @@ if [ "$?" -eq 0 ]; then
 
     # check if supervisord is already running
 
-    if [ -f "run/supervisord.pid" ]; then
-        pid="$(pgrep -F "run/supervisord.pid")"
-        if [ -n "$pid" ] && [ "$pid" -gt 0 ] && [ -S "run/supervisord.sock" ]; then
-            echo " ++ warn ($0): supervisord($pid) is already running (from $PWD/run/supervisord.pid)" >&2
+    if [ -f "$PWD/run/supervisord.pid" ]; then
+        pid="$(pgrep -F "$PWD/run/supervisord.pid" 2>/dev/null)"
+        if [ -n "$pid" ] && [ "$pid" -gt 0 ] && [ -S "$PWD/run/supervisord.sock" ]; then
             return 0
         fi
     fi
 
     # evaluate local .env when exists before supervisord
 
-    if [ -x $commands[direnv] ]; then
+    if [ -f "$PWD/.envrc" ] && [ -x "$commands[direnv]" ]; then
         eval "$(direnv export zsh)"
     fi
 
+    if [ ! -x "$commands[uvx]" ]; then
+        echo " == fail ($0): uvx not found"
+        return 1
+    fi
+
     # start supervisord and check if it is started successfully
-    uvx --from supervisor supervisord --silent --configuration $PWD/supervisord.conf && sleep 0.5
+    uvx \
+        --color always \
+        --frozen \
+        --no-progress \
+        --no-sync \
+        --quiet \
+        --from supervisor \
+        supervisord \
+            --silent \
+            --configuration "$PWD/supervisord.conf" \
+    && sleep 0.5
 
     if [ "$?" -eq 0 ] && [ -f "run/supervisord.pid" ]; then
         pid="$(pgrep -F "run/supervisord.pid")"
